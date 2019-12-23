8298c5bd7ea7b57cb993bbed10a05a0971b22b82
cyberjso <jackson.oliveira.rs@gmail.com>
2019-12-23 00:35:01 -0300

    refactoring

--- resilience4j-playground/src/main/java/io/jackson/oliveira/rs/resilience4j/playground/Main.java
+++
@@ -1,9 +0,0 @@
-package io.jackson.oliveira.rs.resilience4j.playground;
-
-public class Main {
-	
-	public static void main(String[] args) {
-		System.out.println(new CircuiteBreaker().buildCircuiteWithCustomConfigs());
-	}
-
-}
