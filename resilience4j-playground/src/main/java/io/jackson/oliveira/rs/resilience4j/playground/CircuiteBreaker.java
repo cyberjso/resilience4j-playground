2ce31ec7304162951a58e3bc8eb293cc2edec611
cyberjso <jackson.oliveira.rs@gmail.com>
2019-12-23 00:32:03 -0300

    Circuit breaker with non default configs

--- resilience4j-playground/src/main/java/io/jackson/oliveira/rs/resilience4j/playground/CircuiteBreaker.java
+++ resilience4j-playground/src/main/java/io/jackson/oliveira/rs/resilience4j/playground/CircuiteBreaker.java
@@ -1,12 +1,15 @@
 package io.jackson.oliveira.rs.resilience4j.playground;
 
+import java.io.IOException;
+import java.time.Duration;
+
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import io.vavr.CheckedFunction0;
 import io.vavr.control.Try;
 
 public class CircuiteBreaker {
-	
 	
 	public String helloCircuit() {
 	
@@ -22,10 +25,28 @@ public class CircuiteBreaker {
 		return  circuitBreaker.executeSupplier(() -> apiCaller.callExternalAPI("1"));		
 	}
 	
-	
 	public String recoverFromExeception() {
 		CircuitBreaker circuitBreaker = CircuitBreaker.ofDefaults("recoverableCircuit");
 
+		CheckedFunction0<String> supplier =  CircuitBreaker.decorateCheckedSupplier(circuitBreaker, () -> new ApiCaller().callForcingExceptin("1"));
+		
+		return Try.of(supplier).recover(throwable -> "This is the default string, when an exception is thrown").get();
+	}
+	
+	
+	public String buildCircuiteWithCustomConfigs( ) {
+		CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+			    .failureRateThreshold(50)
+			    .waitDurationInOpenState(Duration.ofMillis(1000))
+			    .permittedNumberOfCallsInHalfOpenState(2)
+			    .slidingWindowSize(2)
+			    .recordExceptions(RuntimeException.class)
+			    .ignoreExceptions(IOException.class)
+			    .build();
+		
+		CircuitBreakerRegistry registry = CircuitBreakerRegistry.of(circuitBreakerConfig);
+		CircuitBreaker circuitBreaker = registry.circuitBreaker("circuiteBreakerExample");
+		
 		CheckedFunction0<String> supplier =  CircuitBreaker.decorateCheckedSupplier(circuitBreaker, () -> new ApiCaller().callForcingExceptin("1"));
 		
 		return Try.of(supplier).recover(throwable -> "This is the default string, when an exception is thrown").get();
--- resilience4j-playground/src/main/java/io/jackson/oliveira/rs/resilience4j/playground/Main.java
+++ resilience4j-playground/src/main/java/io/jackson/oliveira/rs/resilience4j/playground/Main.java
@@ -3,7 +3,7 @@ package io.jackson.oliveira.rs.resilience4j.playground;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println(new CircuiteBreaker().recoverFromExeception());
+		System.out.println(new CircuiteBreaker().buildCircuiteWithCustomConfigs());
 	}
 
 }
